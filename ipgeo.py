a9a2bdfd14e114863ca5f92f9ed226da6f60b19f
rias <rias@contrarium.net>
2020-06-07 12:57:37 -0600

    it works!

---
+++ ipgeo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+import socket
+import requests
+
+if len(sys.argv) == 1:
+    print("Please specify an IP address, or 'me' to look up your own current IP.\nUsage: ipgeo <ip address>")
+    sys.exit()
+else:
+    ip = sys.argv[1]
+
+# --- get your own IP address ---
+if not ip or ip in [ 'self', 'mine', 'me', 'my', 'own', 'local', 'current']:
+    r = requests.get('https://curlmyip.org')
+    ip = r.text
+
+# --- validate IP address ---
+try:
+    socket.inet_aton(ip)
+except:
+    print(f"Invalid IP address: {ip}")
+    sys.exit()
+
+# --- get the info ---
+response = requests.get('http://ip-api.com/json/%s' % ip).json()
+print("IP:      %s\nCity:    %s\nRegion:  %s\nCountry: %s" % (ip, response['city'], response['region'], response['country']))
